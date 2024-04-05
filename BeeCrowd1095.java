673349df6ba34757b8d1c65661552fc3913cd1b5
Raissa Rodrigues <105363135+RaihRaih@users.noreply.github.com>
2024-04-05 14:51:11 -0300

    Create BeeCrowd1095.java

---
+++ BeeCrowd1095.java
@@ -0,0 +1,9 @@
+public class BeeCrowd1095 {
+ 
+    public static void main(String[] args){
+ 
+    for(int I = 1, J = 60; J >= 0; I +=3 , J-=5){
+        System.out.println("I="+I+" J="+J);
+        }
+    }
+}
